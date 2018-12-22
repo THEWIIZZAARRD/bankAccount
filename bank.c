536eea76fb282fdd325d61bbc704bc7a2c3eb7dc
new name <39889266+THEWIIZZAARRD@users.noreply.github.com>
2018-12-22 13:19:36 -0800

    Testing out values less than ten

--- bank.c
+++ bank.c
@@ -6,6 +6,7 @@ void begin(){
   
   printf("Enter amount: %d", input);
   
+  findRange(digit);
 }
 
 void findRange(double digit){
@@ -30,4 +31,55 @@ void findRange(double digit){
   }
 }
 // This functions will find the amount the number being inserted is
+
+void less_Than_10(double digit){
+  int getDigitAsInt = (int) digit;
+  int getCents = digit - getDigitAsInt;
+  char holdWord[150];
+
+  switch(digit){
+    case 0: 
+      holdWord = "Zero Dollar";
+      break;
+
+    case 1: 
+      holdWord = "One Dollar ";
+      break;
+    
+    case 2: 
+      holdWord = "Two Dollars ";
+      break; 
+
+    case 3: 
+      holdWord = "Three Dollars";
+      break;
+
+    case 4: 
+      holdWord = "Four Dollars";
+      break;
+    
+    case 5: 
+      holdWord = "Five Dollars";
+      break;
+
+    case 6: 
+      holdWord = "Six Dollars";
+      break;
+    
+    case 7: 
+      holdWord = "Seven Dollars";
+      break;
+    
+    case 8:
+      holdWord = "Eight Dollars";
+      break;
+
+    case 9:
+      holdWord = "Nine Dollars";
+      break;
+  }
+
+
+}
+// Check is cents are 
 
