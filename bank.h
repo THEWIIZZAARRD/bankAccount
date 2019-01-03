04d9a8290ce66be5384e691f35f0b79c5c8e0910
new name <39889266+THEWIIZZAARRD@users.noreply.github.com>
2019-01-02 18:44:57 -0800

    added a new function to work be able to print the cents

--- bank.h
+++ bank.h
@@ -4,13 +4,15 @@
 void begin(); 
 // Begins the program
 
+void printCents(int);
+
 void findRange(double digit);
 
-void less_Than_10(double);
+void less_Than_10(double digit);
 
-void less_Than_100(double);
+void less_Than_100(double digit);
 
-void less_Than_One_THousand(double);
+void less_Than_One_Thousand(double);
 
 void less_Than_Ten_Thousand(double);
 
